479f2329bc8f6574420f717cda1580ce6e8828df
Weike Shi <weikeshih@gmail.com>
2022-02-03 23:32:05 -0500

    fix logo image css

--- components/nav.component.tsx
+++ components/nav.component.tsx
@@ -6,18 +6,19 @@ const Nav = () => {
   return (
     <nav>
       <div className="bg-slate-700 text-slate-200 flex items-center p-2">
-        <div className="relative w-48 h-16 mx-10">
-          <Link href="/">
-            <a>
+        <Link href="/">
+          <a>
+            <div className="relative w-48 h-16 mx-10">
               <Image
+                priority
                 src="/pitstop.png"
                 alt="logo"
                 layout="fill"
                 objectFit="contain"
               />
-            </a>
-          </Link>
-        </div>
+            </div>
+          </a>
+        </Link>
         <div className="mx-5">
           <Link href="/">
             <a>Home</a>
