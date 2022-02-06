5ce51427223cd8e4c6d73a25d1e3c0912ebdc94b
Weike Shi <weikeshih@gmail.com>
2022-02-06 12:51:31 -0500

    add skew to product page

--- components/product.component.tsx
+++ components/product.component.tsx
@@ -10,12 +10,15 @@ interface Product {
 const Product = ({ product, index }: Product) => {
   const { id, name, imageUrl, price } = product;
 
-  const mlauto = index % 2 === 0 ? "" : "ml-auto";
+  const mlauto = index % 2 === 0 ? "" : "ml-auto ";
   const flexrowreverse = index % 2 === 0 ? "flex-row-reverse" : "";
+  const skew = index % 2 === 0 ? "-skew-x-12" : "skew-x-12";
   return (
     <div className="flex justify-center mx-auto w-full">
-      <div className={`flex m-10 w-2/3 bg-slate-200 ${flexrowreverse}`}>
-        <div className="flex relative w-1/3 h-48 justify-center items-center m-auto p-3">
+      <div
+        className={`relative flex m-10 w-2/3 bg-slate-200 ${flexrowreverse}`}
+      >
+        <div className="flex relative w-1/2 h-48 justify-center items-center m-auto p-3">
           <Image
             src={imageUrl}
             className="w-full h-full object-contain"
@@ -25,7 +28,10 @@ const Product = ({ product, index }: Product) => {
             layout="fill"
           />
         </div>
-        <div className="flex flex-col p-10 bg-slate-400 w-2/3">
+        <div
+          className={`absolute left-1/2 -translate-x-1/2 bg-slate-200 w-1/12 h-full z-50 ${skew}`}
+        ></div>
+        <div className={`flex flex-col p-10 bg-slate-400 w-1/2`}>
           <div className={`text-3xl my-3 ${mlauto}`}>{name}</div>
           <div className={`flex items-center gap-3 mt-24 ${mlauto}`}>
             <div className="flex items-end gap-1">
