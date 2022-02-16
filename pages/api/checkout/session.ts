ac60aab36943d0b7aec35759743c46fa37a129d8
Weike Shi <weikeshih@gmail.com>
2022-02-15 22:00:05 -0500

    Add Stripe serverless function

---
+++ pages/api/checkout/session.ts
@@ -0,0 +1,45 @@
+import Stripe from "stripe";
+import type { NextApiRequest, NextApiResponse } from "next";
+import getStripe from "../../../utils/get-stripejs";
+import { formatAmountForStripe } from "../../../utils/stripe-helpers";
+
+// const stripe = getStripe();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2020-08-27",
+});
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const total: number = req.body.total;
+
+    try {
+      const params: Stripe.Checkout.SessionCreateParams = {
+        submit_type: "pay",
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            name: "Custom amount donation",
+            amount: formatAmountForStripe(total, "CAD"),
+            currency: "CAD",
+            quantity: 1,
+          },
+        ],
+        success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+      };
+
+      const checkoutSession: Stripe.Checkout.Session =
+        await stripe.checkout.sessions.create(params);
+      res.status(200).json(checkoutSession);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "Internal server error";
+      res.status(500).json({ statusCode: 500, message: errorMessage });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+};
+
+export default handler;
