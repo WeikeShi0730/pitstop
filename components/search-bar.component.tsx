b66bc470bef33afad7813963e779d5ecdf6faba3
Weike Shi <weikeshih@gmail.com>
2022-02-21 17:59:17 -0500

    Fix input auto focus and animation conflict

--- components/search-bar.component.tsx
+++ components/search-bar.component.tsx
@@ -7,13 +7,17 @@ import { useClickOutside } from "../utils/use-click-outside";
 const SearchBar = () => {
   const [open, setOpen] = useState<boolean>(false);
   const [input, setInput] = useState<string>("");
-  const ref = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
 
-  useClickOutside(ref, () => setOpen(() => !open));
+  useClickOutside(containerRef, () => setOpen(() => !open));
   const handleOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     setOpen(() => !open);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 500);
   };
 
   const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
@@ -47,12 +51,12 @@ const SearchBar = () => {
         leaveFrom="scale-x-100 opacity-100"
         leaveTo="scale-x-90 opacity-0"
       >
-        <div ref={ref} className="flex justify-center items-center">
+        <div ref={containerRef} className="flex justify-center items-center">
           <form onSubmit={handleSubmit}>
             <input
+              ref={inputRef}
               className="bg-transparent outline-none border-b border-orange-theme w-36"
               autoComplete="off"
-              autoFocus={true}
               type="text"
               name="name"
               placeholder="Search..."
