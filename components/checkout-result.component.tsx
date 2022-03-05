e25892dbfe66e2d862336e2404eb389e0b03cec3
Weike Shi <weikeshih@gmail.com>
2022-03-05 14:08:25 -0500

    Rafactor checkout result page

--- components/checkout-result.component.tsx
+++ components/checkout-result.component.tsx
@@ -1,33 +1,73 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import {
+  clearCartFirebase,
+  updateOrderHistory,
+  updateItemsSoldNum,
+} from "../firebase/firebase.utils";
+import { useRouter } from "next/router";
+import { fetchGetJSON } from "../utils/api-helpers";
+import useSWR from "swr";
 import Loading from "./loading.component";
 
-const CheckoutResult = ({ data }: any) => {
-  const view = data ? (
-    data?.payment_intent?.status === "succeeded" ? (
-      <>
-        <div className="text-4xl">🥳</div>
-        <div className="text leading-8 text-center">
-          Your order has been confirmed!
-        </div>
-      </>
-    ) : (
+const CheckoutResult = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+  // Fetch CheckoutSession from static page via
+  // https://nextjs.org/docs/basic-features/data-fetching#static-generation
+  const { data, error } = useSWR(
+    router.query.session_id &&
+      `/api/checkout-session/${router.query.session_id}`,
+    fetchGetJSON
+  );
+  const status = data?.payment_intent?.status;
+
+  useEffect(() => {
+    const clearCart = async () => {
+      if (status === "succeeded") {
+        try {
+          setLoading(true);
+          await Promise.all([
+            updateOrderHistory(),
+            updateItemsSoldNum(),
+            clearCartFirebase(),
+          ]);
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
+          console.error(error);
+        }
+      }
+    };
+    clearCart();
+  }, [status]);
+
+  const view =
+    data &&
+    (data?.payment_intent?.status !== "succeeded" || error ? (
       <>
         <div className="text-4xl">😭</div>
         <div className="text leading-8 text-center">
           Your order didn&apos;t go through, please try again!
         </div>
       </>
-    )
-  ) : (
-    <Loading />
-  );
+    ) : (
+      <>
+        <div className="text-4xl">🥳</div>
+        <div className="text leading-8 text-center">
+          Your order has been confirmed!
+        </div>
+      </>
+    ));
 
   return (
-    <div className="flex justify-center items-center h-content">
-      <div className="flex flex-col justify-center items-center max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg m-auto gap-y-5 p-5 rounded-lg text-slate-700 bg-opacity-80 backdrop-blur-md bg-slate-50 shadow-2xl">
-        {view}
+    <>
+      {(loading || !data) && <Loading />}
+      <div className="flex justify-center items-center h-content">
+        <div className="flex flex-col justify-center items-center max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg m-auto gap-y-5 p-5 rounded-lg text-slate-700 bg-opacity-80 backdrop-blur-md bg-slate-50 shadow-2xl">
+          {view}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,52 +1,11 @@
 import { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
 import Layout from "../../components/layout.component";
-import {
-  clearCartFirebase,
-  updateOrderHistory,
-  updateItemsSoldNum,
-} from "../../firebase/firebase.utils";
 import CheckoutResult from "../../components/checkout-result.component";
 
-import { fetchGetJSON } from "../../utils/api-helpers";
-import useSWR from "swr";
-
 const ResultPage: NextPage = () => {
-  const router = useRouter();
-
-  // Fetch CheckoutSession from static page via
-  // https://nextjs.org/docs/basic-features/data-fetching#static-generation
-  const { data, error } = useSWR(
-    router.query.session_id &&
-      `/api/checkout-session/${router.query.session_id}`,
-    fetchGetJSON
-  );
-  const status = data?.payment_intent?.status;
-
-  useEffect(() => {
-    const clearCart = async () => {
-      if (status === "succeeded") {
-        try {
-          await Promise.all([
-            updateOrderHistory(),
-            updateItemsSoldNum(),
-            clearCartFirebase(),
-          ]);
-          // await clearCartFirebase();
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    };
-    clearCart();
-  }, [status]);
-  if (error) return <div>failed to load</div>;
-
   return (
     <Layout title="Pitstop | Confirmation">
-      <CheckoutResult data={data} />
+      <CheckoutResult />
     </Layout>
   );
 };
