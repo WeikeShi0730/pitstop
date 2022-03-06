906e93f11ce390ab37f56213e19b66669982f788
Weike Shi <weikeshih@gmail.com>
2022-03-06 18:53:38 -0500

    Fix checkout page css

--- components/checkout.component.tsx
+++ components/checkout.component.tsx
@@ -7,6 +7,12 @@ import { FaCcStripe } from "react-icons/Fa";
 import withSubscribtion from "./hoc.component";
 import Loading from "./loading.component";
 import NoScrollLink from "./no-scroll-link.component";
+import {
+  SiAmericanexpress,
+  SiVisa,
+  SiMastercard,
+  SiStripe,
+} from "react-icons/si";
 
 interface CartItems {
   cartItems: CartItemType[] | null;
@@ -55,7 +61,7 @@ const Checkout = ({ cartItems }: CartItems) => {
   return (
     <>
       {loading && <Loading />}
-      <div className="relative text-slate-700 min-h-content w-full flex flex-col lg:flex-row lg:justify-center">
+      <div className="relative text-slate-700 min-h-content w-full flex flex-col lg:flex-row justify-center">
         <div className="relative w-full lg:w-3/4 my-10">
           <div className="flex justify-center m-2">
             <div className="flex w-full sm:w-2/3 m-5 justify-start text-2xl py-1 border-b border-slate-700">
@@ -94,15 +100,23 @@ const Checkout = ({ cartItems }: CartItems) => {
             </div>
           )}
         </div>
-        <div className="w-full lg:w-1/4 flex mb-10 justify-center lg:items-start ">
-          <div className="fixed bottom-0 z-40 w-full bg-slate-50 bg-opacity-50 backdrop-blur-md lg:bg-transparent lg:sticky lg:top-24">
-            <div className="flex flex-row lg:flex-col items-center justify-center">
+        <div className="w-full lg:w-1/4 flex lg:my-10 justify-center lg:items-start ">
+          <div className="fixed bottom-0 z-40 w-full bg-slate-50 bg-opacity-50 backdrop-blur-md lg:bg-transparent lg:sticky lg:top-32">
+            <div className="flex flex-row lg:flex-col items-center justify-evenly">
+              {cartItems && cartItems.length > 0 && (
+                <div className="lg:flex">
+                  <div className="flex w-full lg:w-48 xl:w-60 2xl:w-72 justify-center lg:justify-end items-end my-2 mx-1 lg:m-5 space-x-2 py-1 lg:border-b border-slate-700">
+                    <div className="lg:text-lg">Total: CAD</div>
+                    <div className="text-lg lg:text-2xl">{total}</div>
+                  </div>
+                </div>
+              )}
               <form onSubmit={handleSubmit} method="POST">
                 <button
                   role="link"
                   id="submit"
                   type="submit"
-                  className="flex justify-center items-center w-48 my-2 mx-1 lg:m-5 rounded-lg bg-indigo-600 text-slate-200 hover:text-slate-50 hover:bg-indigo-500"
+                  className="flex justify-center items-center w-48 xl:w-60 2xl:w-72 my-2 mx-1 lg:m-5 rounded-lg bg-indigo-600 text-slate-200 hover:text-slate-50 hover:bg-indigo-500"
                 >
                   <div className="m-1">Checkout with</div>
                   <div className="m-1 ">
@@ -110,14 +124,23 @@ const Checkout = ({ cartItems }: CartItems) => {
                   </div>
                 </button>
               </form>
-              {cartItems && cartItems.length > 0 && (
-                <div className="lg:flex">
-                  <div className="flex w-full lg:w-48 justify-center lg:justify-end items-end my-2 mx-1 lg:m-5 space-x-2 py-1 lg:border-t border-slate-700">
-                    <div className="lg:text-lg">Total: CAD</div>
-                    <div className="text-lg lg:text-2xl">{total}</div>
+              <div className="cards w-48 xl:w-60 2xl:w-72 my-2 mx-1 lg:m-5 hidden lg:flex flex-col items-start justify-center">
+                <div className="m-1">Payment methods</div>
+                <div className="flex gap-2">
+                  <div className="text-2xl">
+                    <SiVisa />
+                  </div>
+                  <div className="text-2xl">
+                    <SiMastercard />
+                  </div>
+                  <div className="text-2xl">
+                    <SiAmericanexpress />
+                  </div>
+                  <div className="text-2xl">
+                    <SiStripe />
                   </div>
                 </div>
-              )}
+              </div>
             </div>
           </div>
         </div>
