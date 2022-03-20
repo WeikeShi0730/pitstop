8c7156823f2a9e888dc4f4eff0015b5a7a7a7911
Weike Shi <weikeshih@gmail.com>
2022-03-20 01:10:54 -0400

    fix sorting icon

--- components/sorting-bar.component.tsx
+++ components/sorting-bar.component.tsx
@@ -1,4 +1,5 @@
 import { Tab } from "@headlessui/react";
+import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 
 interface SortingBarType {
   handleChange: (index: number) => void;
@@ -6,7 +7,7 @@ interface SortingBarType {
 
 const SortingBar = ({ handleChange }: SortingBarType) => {
   const tabStyle = (selected: boolean) => {
-    return `font-light w-full p-1 leading-5 text-slate-700 rounded-lg focus:outline-none transition-all ease-in-out duration-200 ${
+    return `font-light w-full p-1 leading-5 flex justify-center items-center text-slate-700 rounded-lg focus:outline-none transition-all ease-in-out duration-200 ${
       selected
         ? "bg-orange-theme shadow text-slate-100"
         : "hover:bg-slate-100 text-slate-700"
@@ -17,10 +18,16 @@ const SortingBar = ({ handleChange }: SortingBarType) => {
       <Tab.Group onChange={(index) => handleChange(index)}>
         <Tab.List className="flex w-[21rem] p-1 gap-x-1 bg-slate-300 rounded-lg shadow">
           <Tab className={({ selected }) => tabStyle(selected)}>Popular</Tab>
-          <Tab className={({ selected }) => tabStyle(selected)}>Price ⤴</Tab>
-          <Tab className={({ selected }) => tabStyle(selected)}>Price ⤵</Tab>
-          <Tab className={({ selected }) => tabStyle(selected)}>A - Z</Tab>
-          <Tab className={({ selected }) => tabStyle(selected)}>Z - A</Tab>
+          <Tab className={({ selected }) => tabStyle(selected)}>
+            Price
+            <AiOutlineArrowUp />
+          </Tab>
+          <Tab className={({ selected }) => tabStyle(selected)}>
+            Price
+            <AiOutlineArrowDown />
+          </Tab>
+          <Tab className={({ selected }) => tabStyle(selected)}>A-Z</Tab>
+          <Tab className={({ selected }) => tabStyle(selected)}>Z-A</Tab>
         </Tab.List>
       </Tab.Group>
     </div>
