f7d104f8007fc5397521d4caef1175c726fdaddf
Weike Shi <weikeshih@gmail.com>
2022-03-31 00:05:56 -0400

    Added test for <CartIcon />

--- __test__/cart-icon.test.tsx
+++ __test__/cart-icon.test.tsx
@@ -1,4 +1,9 @@
-import { render, RenderResult } from "@testing-library/react";
+import {
+  render,
+  RenderResult,
+  screen,
+  fireEvent,
+} from "@testing-library/react";
 import CartIcon from "../components/cart-icon.component";
 
 let documentBody: RenderResult;
@@ -43,5 +48,13 @@ describe("<CartIcon />", () => {
       />
     );
     expect(documentBody.queryByText("15")).toBeInTheDocument();
+  });
+
+  it("shows <CartDropdown /> on click", () => {
+    documentBody = render(<CartIcon cartItems={[]} />);
+    expect(screen.queryByText("Checkout")).not.toBeInTheDocument();
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(screen.queryByText("Checkout")).toBeInTheDocument();
   });
 });
