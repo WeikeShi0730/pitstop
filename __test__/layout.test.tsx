f13b1095bda95aeacb8350963909349790a43ce5
Weike Shi <weikeshih@gmail.com>
2022-04-03 19:33:29 -0400

    Add test <Layout />

--- __test__/layout.test.tsx
+++ __test__/layout.test.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/layout.component";
 let documentBody: RenderResult;
 describe("<Layout />", () => {
   beforeEach(() => (documentBody = render(<Layout title="test" />)));
-  it("shows basic fields in <Layout />", () => {
+  it("Test <Layout />", () => {
     const { baseElement } = documentBody;
     expect(baseElement).toMatchSnapshot();
   });
