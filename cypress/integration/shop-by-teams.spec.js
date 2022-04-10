19e1e66953920b11d5e4a912f31dbc8bc43795b1
Weike Shi <weikeshih@gmail.com>
2022-04-09 23:46:05 -0400

    Add Cypress for Shop by teams

---
+++ cypress/integration/shop-by-teams.spec.js
@@ -0,0 +1,34 @@
+describe("Shop by teams", () => {
+  it("user can shop teams products and add to cart", () => {
+    cy.visit("http://localhost:3000/");
+
+    // Sign in
+    cy.findByRole("link", { name: /sign in/i }).click();
+
+    cy.findByRole("textbox", { name: /signInEmail/i }).type("cy@cy.com");
+    cy.get("#signInPassword").type("cycycy");
+
+    cy.get("#signInButton").click();
+
+    // Shop by teams
+    cy.findByRole("link", { name: /shop by teams/i }).click();
+    // Sort
+    cy.findByRole("link", { name: /shop mclaren/i }).click();
+    // Add
+    cy.get("#addToCart").first().click();
+
+    // Cart dropdown
+    cy.get("#cartIcon").click();
+    // Checkout page
+    cy.findByRole("link", { name: /checkout/i }).click();
+
+    // Should contain products
+    cy.contains("Lando Norris' helmet");
+    // Delete items
+    // cy.get("#delete").click();
+
+    // Sign out
+    cy.findByRole("link", { name: /cy/i }).click();
+    cy.findByRole("tab", { name: /sign out/i }).click();
+  });
+});
