ec15830867b308d8d3d053fbcec7432eaa5c302e
Weike Shi <weikeshih@gmail.com>
2022-04-27 21:41:41 -0400

    Update test for <SignOut />

--- __test__/sign-out.test.tsx
+++ __test__/sign-out.test.tsx
@@ -1,7 +1,5 @@
 import { render, RenderResult, fireEvent } from "@testing-library/react";
 import SignOut from "../components/sign-out.component";
-import createMockRouter from "../test-utils/createMockRouter";
-import { RouterContext } from "next/dist/shared/lib/router-context";
 
 let documentBody: RenderResult;
 describe("<SignOut />", () => {
